80ec363597ed060d53caa104c1fac46382e49a84
cantkcu <can.tkcu@hotmail.com>
2023-01-26 14:46:56 +0100

    basic data read functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  title: any;
+  todos$: Observable<any>;
+  todos: Array<any>;
+  todotext = '';
+
+  constructor(private firestore: Firestore) {
+    const collec = collection(firestore, 'todos');
+    this.todos$ = collectionData(collec);
+
+    this.todos$.subscribe((newTodos) => {
+      console.log('Neue Todos sind', newTodos);
+      this.todos = newTodos;
+    });
+  }
+
+  addNewTodo() {
+    const collec = collection(this.firestore, 'todos');
+    setDoc(doc(collec), {
+      name: this.todotext,
+    });
+    this.todotext = '';
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'cant-todolist',
+    appId: '1:490612571576:web:770acea798f76eed4d634d',
+    storageBucket: 'cant-todolist.appspot.com',
+    apiKey: 'AIzaSyCkZTcO9qK4VOrS-hE_m5velzgv73nGAWY',
+    authDomain: 'cant-todolist.firebaseapp.com',
+    messagingSenderId: '490612571576',
+  },};
